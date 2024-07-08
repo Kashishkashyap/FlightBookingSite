d973e9794e7779da62d0aaa28c49c2cac8a56872
Kashish <kashyapkashish22@gmail.com>
2024-07-08 13:01:56 +0530

    revised hw code

--- homework.js
+++ homework.js
@@ -1,4 +1,29 @@
 console.log("starting");
+
+function pause(milliseconds) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+async function counter() {
+    let count = 0;
+    while (count < 10) {
+        count += 2;
+        if (count == 10) {
+            console.log('Execution completed');
+        } else {
+            console.log(`${count} seconds completed`);
+        }
+        await pause(2000);
+    }
+}
+
+counter();
+
+
+/*
+console.log("starting");
 function abc() {
     return new Promise((resolve) => {
         let count = 0;
@@ -17,5 +42,5 @@ function abc() {
 }
 
 abc();
-
+*/
 // I wrapped my code inside abc function and then since I used count inside the function it resolved my closure scope issue.
